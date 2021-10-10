14f7e8e91ee38fcc19fb0775dcb296ee7d5257b5
ClymistAstern <68793983+Clymistastern@users.noreply.github.com>
2021-10-10 16:55:05 +0700

    Problem10

---
+++ 10 problem solve game/Assets/Problem10/scenecontrollers.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+public class scenecontrollers : MonoBehaviour
+{
+    public void Pro1()
+    {
+        SceneManager.LoadScene("Problem1");
+    }
+
+    public void Pro2()
+    {
+        SceneManager.LoadScene("Problem2");
+    }
+
+    public void Pro3()
+    {
+        SceneManager.LoadScene("Problem3");
+    }
+
+    public void Pro4()
+    {
+        SceneManager.LoadScene("Problem4");
+    }
+
+    public void Pro5()
+    {
+        SceneManager.LoadScene("Problem5");
+    }
+
+    public void Pro6()
+    {
+        SceneManager.LoadScene("Problem6");
+    }
+
+    public void Pro7()
+    {
+        SceneManager.LoadScene("Problem7");
+    }
+
+    public void Pro8()
+    {
+        SceneManager.LoadScene("Problem8");
+    }
+
+    public void Pro9()
+    {
+        SceneManager.LoadScene("Problem9");
+    }
+}
